6719ea436abb09a395f87f28983bdd26bf67626d
Jeremy Bell <jeremy.bell91@gmail.com>
2021-04-03 09:13:14 +1000

    Added new, update, delete routes

--- src/routes.js
+++ src/routes.js
@@ -98,4 +98,66 @@ routes.route("/news_headlines/search/:q").get(async (req, res) => {
 
 
 
+
+// New document 
+routes.route("/news_headlines/new").post(async (req, res) => {
+    try {
+        const newDoc = await client.index({
+             index: "news_headlines",
+             type: "_doc",
+             body: {
+                 category: "TRAVEL",
+                 headline: "This is a test, search for MaroonSleepyGorilla",
+                 authors: "Jeremy Bell",
+                 link: "https://www.huffingtonpost.com/entry/volcano-smiling-hawaii_us_579bda86e14b0e2e15eb5fd73",
+                 short_description: "This is a test, RedDancingGiraffe",
+                 date: "2020-07-30"
+             }
+         })
+     
+         console.log(res.json(newDoc))
+        
+    } catch (error) {
+        console.error(res.json(error));
+    }
+})
+
+
+// Update document 
+routes.route("/news_headlines/update/:id").patch(async (req, res) => {
+    try {       
+        const updatedDoc = await client.update({
+             index: "news_headlines",
+             id: req.params.id,
+            body: {
+                doc: {
+                    headline: "This is a test to update, search for TealSleepyGorilla"
+                }
+            }
+         })
+     
+         console.log(res.json(updatedDoc))
+        
+    } catch (error) {
+        console.error(res.json(error));
+    }
+ })
+
+
+// Delete document 
+routes.route("/news_headlines/remove/:id").delete(async (req, res) => {
+    try {       
+        const deletedDoc = await client.delete({
+             index: "news_headlines",
+             id: req.params.id,
+         })
+     
+         console.log(res.json(deletedDoc))
+        
+    } catch (error) {
+        console.error(res.json(error));
+    }
+ })
+
 module.exports = routes;
+//
